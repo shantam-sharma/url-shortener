f3461d6b0e3df282c0ffe66d70094c7daf637c99
shantam-sharma <writetoshantam@gmail.com>
2026-06-28 23:43:59 +0530

    feat: initialize database connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,2 +1,31 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+	"github.com/shantam-sharma/url-shortner/internal/database"
+)
+
+func main() {
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Connect to PostgreSQL
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+	defer db.Close()
+
+	log.Println("Connected to PostgreSQL")
+
+	// Start HTTP server
+	log.Println("Server running on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
--- internal/database/database.go
+++ internal/database/database.go
@@ -1,7 +1,39 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"os"
 
-func connect() (*sql.DB, error) {
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
 
+func Connect() (*sql.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	sslMode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host,
+		port,
+		user,
+		password,
+		dbName,
+		sslMode,
+	)
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
