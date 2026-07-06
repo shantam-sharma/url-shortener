227d4b432e3e1b66c5801d2d5b477802e5d21524
shantam-sharma <writetoshantam@gmail.com>
2026-07-06 21:22:30 +0530

    feat: final finished url shortner working model

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/shantam-sharma/url-shortner/internal/database"
 	"github.com/shantam-sharma/url-shortner/internal/handlers"
+	"github.com/shantam-sharma/url-shortner/internal/middleware"
 	"github.com/shantam-sharma/url-shortner/internal/repository"
 	"github.com/shantam-sharma/url-shortner/internal/service"
 )
@@ -35,8 +36,12 @@ func main() {
 	http.HandleFunc("/api/v1/urls", urlHandler.CreateURL)
 	http.HandleFunc("/", urlHandler.RedirectURL)
 	// Start HTTP server
+	// Start HTTP server
 	log.Println("Server running on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+
+	handler := middleware.CORS(http.DefaultServeMux)
+
+	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
 }
