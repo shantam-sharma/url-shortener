b7e233b224791d65e7a0814f154859af5dcd405c
shantam-sharma <writetoshantam@gmail.com>
2026-07-09 23:20:41 +0530

    Fix CORS for production frontend

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,11 +1,19 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		origin := r.Header.Get("Origin")
+
+		if origin == os.Getenv("FRONTEND_URL") || origin == "http://localhost:5173" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
